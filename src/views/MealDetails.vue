4e62b27ad3401d7cff569197808aa44284f234f1
Reynaldo Ibarra <ibarra.rey02@gmail.com>
2025-11-21 21:01:23 -0600

    Meal Details working

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,4 +1,24 @@
 <script setup>
+import { onMounted, ref } from 'vue';
+import useApi from '@/composables/useApi';
+import { useRoute } from 'vue-router';
+
+const { api } = useApi();
+const route = useRoute();
+const meal = ref();
+
+onMounted(() => {
+    api.get(`/lookup.php?i=${route.params.id}`)
+        .then(({ data }) => {
+            meal.value = data.meals[0] || {}
+        });
+
+})
+
 </script>
 
-<template></template>
+<template>
+    <div class="">
+        {{ meal }}
+    </div>
+</template>
