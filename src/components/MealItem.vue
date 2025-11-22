63e9298b6344072c767eed101633954f96870f25
Reynaldo Ibarra <ibarra.rey02@gmail.com>
2025-11-21 20:27:59 -0600

    homepage working

---
+++ src/components/MealItem.vue
@@ -0,0 +1,26 @@
+<script setup>
+const { meal } = defineProps({
+    meal: {
+        type: Object,
+        required: true
+    }
+
+})
+
+</script>
+
+<template>
+    <div class="bg-white shadow rounded-xl">
+        <router-link :to="{}">
+            <img :src="meal.strMealThumb" class="rounded-t-xl h-48 object-cover w-full" />
+        </router-link>
+
+        <div class="p-3">
+            <h3 class="font-bold">{{ meal.strMealThumb }}</h3>
+            <p class="mb-4 line-clamp-3">{{ meal.strInstructions }}</p>
+        </div>
+
+    </div>
+
+
+</template>
